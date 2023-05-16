7a10880fef7b2a6efa2bf4ffd14c9cb7dfadba14
Leonardo Rocha <121068523+leosandaro@users.noreply.github.com>
2023-05-16 18:33:24 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+// esse é um comentário
+
+/*
+Esse é um comentário
+multi-linhas
+*/
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    
+    body.classList.toggle("modo-escuro");
+
+    if(modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/sun.png");
+    }
+    else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/moon.png");
+    }
+
+});
+
+
+
